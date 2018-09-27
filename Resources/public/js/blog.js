bb98098e6343e958c51b284ee4c5d90ede53cbaa
Kévin Hilairet <kevin@octopouce.mu>
2018-09-27 11:22:32 +0400

    update

--- Resources/public/js/blog.js
+++ Resources/public/js/blog.js
@@ -1,6 +1,4 @@
 $(document).ready(function () {
-    ClassicEditor.create( document.querySelector( '.editor' ) );
-
     $('input#post_seoTitle, input#post_seoDescription, ' +
         'input#post_seoFacebookTitle, input#post_seoFacebookDescription, ' +
         'input#post_seoTwitterTitle, input#post_seoTwitterDescription')
